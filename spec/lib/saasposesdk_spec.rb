bab421baa5913a896e7c721eb4ad6c7dba01d624
Peter Schröder <phoetmail@googlemail.com>
2012-04-17 20:29:30 +0200

    fix example spec

--- spec/lib/saasposesdk_spec.rb
+++ spec/lib/saasposesdk_spec.rb
@@ -1,15 +1,27 @@
 require "spec_helper"
 
 describe "saasposesdk" do
+  PNG_PATH = "/tmp/test.png"
+  TEST_PDF_NAME = "saaspose_test.pdf"
+
+  let(:pdf_converter) { Pdf::Convertor.new(TEST_PDF_NAME) }
+  let(:test_pdf_path) { File.expand_path("../fixtures/#{TEST_PDF_NAME}", File.dirname(__FILE__)) }
+
   before(:all) do
     Common::Product.setBaseProductUri("http://api.saaspose.com/v1.0")
     Common::SaasposeApp.new(ENV["SAASPOSE_APPSID"], ENV["SAASPOSE_APPKEY"])
+
+    unless Storage::Folder.getFiles("").map(&:Name).include?(TEST_PDF_NAME)
+      puts "uploading #{TEST_PDF_NAME} for testing purposes"
+      Storage::Folder.uploadFile(test_pdf_path, "")
+    end
+    FileUtils.rm(PNG_PATH) if File.exists?(PNG_PATH)
   end
 
   context "pdf" do
     it "should generate a pdf" do
-      pdfConvertor = Pdf::Convertor.new('test_data/test.pdf')
-      pdfConvertor.convert('/tmp/test.png','png','1','800','600')
+      pdf_converter.convert(PNG_PATH,'png','1','800','600')
+      File.exists?(PNG_PATH).should be_true
     end
   end
 end
