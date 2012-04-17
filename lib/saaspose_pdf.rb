72967010f5610a4cd79b0b68f7aa02eb84b256a2
Peter Schröder <phoetmail@googlemail.com>
2012-04-17 21:26:31 +0200

    delete whitespace

--- lib/saaspose_pdf.rb
+++ lib/saaspose_pdf.rb
@@ -5,18 +5,18 @@ module Pdf
     # This class provides functionality for converting PDF Documents to other supported formats.
     class Convertor
 	    # Constructor for the Convertor Class.
-        # * :name represents the name of the PDF Document on the Saaspose server 		
-        def initialize(name)  
-           # Instance variables   
+        # * :name represents the name of the PDF Document on the Saaspose server
+        def initialize(name)
+           # Instance variables
            @name = name
         end
 	     # Converts the file available at Saaspose Storage and saves converted file locally.
 		 # * :localFile represents converted local file path and name
-         # * :saveImageFormat represents the converted image format. For a list of supported image formats, please visit 
+         # * :saveImageFormat represents the converted image format. For a list of supported image formats, please visit
 		 #  http://saaspose.com/docs/display/pdf/page
          # * :pageNumber represents the page number in the PDF document
          # * :height represents height of the converted image
-         # * :width represents width of the converted image 		 
+         # * :width represents width of the converted image
         def convert(localFile,saveImageFormat,pageNumber,height,width)
 		    urlDoc = $productURI + '/pdf/' + @name + '/pages/' + pageNumber + '?format=' + saveImageFormat + '&width=' + width + '&height=' + height
 		    signedURL = Common::Utils.sign(urlDoc)
@@ -28,7 +28,7 @@ module Pdf
 		    urlPage = $productURI + '/pdf/' + @name + '/pages'
 			signedURL = Common::Utils.sign(urlPage)
 			count = Common::Utils.getFieldCount(signedURL,'/SaaSposeResponse/Pages/Page')
-			return count 
-        end		
-    end  
+			return count
+        end
+    end
 end
--- lib/saaspose_words.rb
+++ lib/saaspose_words.rb
@@ -3,22 +3,22 @@ require_relative 'saaspose_storage'
 # This module provide wrapper classes to work with Saaspose.Words resources
 module Words
     # This class provides functionality for converting Word Documents to other supported formats.
-    class Convertor  
+    class Convertor
         # Constructor for the Convertor Class.
-        # * :name represents the name of the Word Document on the Saaspose server 		
-	    def initialize(name)  
-             # Instance variables   
+        # * :name represents the name of the Word Document on the Saaspose server
+	    def initialize(name)
+             # Instance variables
              @name = name
         end
 	     # Converts the file available at Saaspose Storage and saves converted file locally.
 		 # * :localFile represents converted local file path and name
-         # * :saveFormat represents the converted format. For a list of supported formats, please visit 
-		 #  http://saaspose.com/docs/display/words/document		 
+         # * :saveFormat represents the converted format. For a list of supported formats, please visit
+		 #  http://saaspose.com/docs/display/words/document
         def convert(localFile,saveFormat)
 		    urlDoc = $productURI + '/words/' + @name + '?format=' + saveFormat
 		    signedURL = Common::Utils.sign(urlDoc)
 		    response = RestClient.get(signedURL, :accept => 'application/json')
 			Common::Utils.saveFile(response,localFile)
-        end		
-    end  
+        end
+    end
 end
