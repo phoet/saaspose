248ff36a1c084b26d962b14021fcb48c7b63a795
Peter Schröder <phoetmail@googlemail.com>
2012-04-26 21:05:08 +0200

    add spec for slides

--- spec/lib/saasposesdk_spec.rb
+++ spec/lib/saasposesdk_spec.rb
@@ -2,24 +2,33 @@ require "spec_helper"
 
 describe "saasposesdk" do
   PNG_PATH = "/tmp/test.png"
+  PDF_PATH = "/tmp/test.pdf"
   TEST_PDF_NAME = "saaspose_test.pdf"
+  TEST_PPT_NAME = "saaspose_test.ppt"
 
   let(:pdf_converter) { Pdf::Convertor.new(TEST_PDF_NAME) }
-  let(:test_pdf_path) { File.expand_path("../fixtures/#{TEST_PDF_NAME}", File.dirname(__FILE__)) }
+  let(:ppt_converter) { Slides::Convertor.new(TEST_PPT_NAME) }
 
   before(:all) do
     configure_client
-    ensure_remote_file
+    [TEST_PDF_NAME, TEST_PPT_NAME].each { |path| ensure_remote_file(path) }
+  end
+
+  before(:each) do
+    [PNG_PATH, PDF_PATH].each { |path| FileUtils.rm(path) if File.exists?(path) }
   end
 
   context "pdf" do
-    before(:each) do
-      FileUtils.rm(PNG_PATH) if File.exists?(PNG_PATH)
-    end
-
-    it "should generate a pdf", :vcr => true do
-      pdf_converter.convert(PNG_PATH,'png','1','800','600')
+    it "should generate a png from a remote pdf", :vcr => true do
+      pdf_converter.convert(PNG_PATH, 'png', '1', '800', '600')
       File.exists?(PNG_PATH).should be_true
+    end
+  end
+
+  context "slides" do
+    it "should generate a pdf from a remote ppt", :vcr => true do
+      ppt_converter.convert(PDF_PATH, 'pdf')
+      File.exists?(PDF_PATH).should be_true
     end
   end
 end
@@ -29,11 +38,11 @@ def configure_client
   Common::SaasposeApp.new(ENV["SAASPOSE_APPSID"], ENV["SAASPOSE_APPKEY"])
 end
 
-def ensure_remote_file
-  VCR.use_cassette("ensure_remote_file", :record => :new_episodes, :match_requests_on => [:host, :path]) do
-    unless Storage::Folder.getFiles("").map(&:Name).include?(TEST_PDF_NAME)
-      puts "uploading #{TEST_PDF_NAME} for testing purposes"
-      Storage::Folder.uploadFile(test_pdf_path, "")
+def ensure_remote_file(test_file)
+  VCR.use_cassette("ensure_remote_file #{test_file}", :record => :new_episodes, :match_requests_on => [:host, :path]) do
+    unless Storage::Folder.getFiles("").map(&:Name).include?(test_file)
+      puts "uploading #{test_file} for testing purposes"
+      Storage::Folder.uploadFile(File.expand_path("../fixtures/#{test_file}", File.dirname(__FILE__)), "")
     end
   end
 end
